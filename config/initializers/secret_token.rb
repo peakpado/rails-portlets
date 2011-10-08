f7f8f5d10d2538799834c57596f65bdb606d929c
Peak Pado <peakpado@gmail.com>
2011-10-08 11:56:42 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsPortlets::Application.config.secret_token = 'c747f8adf9295826d174b3722248c877a070f7ff09ec0f6f350b98904e62b4ba5e2f589c7e9759e349e68df0e4bf00e58ff748eb5b9c1632a34addfd46a8679c'
